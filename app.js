8a16dae9295d7e10e9f4fe473032e3f2622b646a
Margarida <margarida97pt@gmail.com>
2020-11-11 17:54:18 +0000

    done

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 require('dotenv').config();
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+    const bodyParser = require('body-parser');
+    const cookieParser = require('cookie-parser');
+    const express = require('express');
+    const favicon = require('serve-favicon');
+    const hbs = require('hbs');
+    const mongoose = require('mongoose');
+    const logger = require('morgan');
+    const path = require('path');
+    
+    const app_name = require('./package.json').name;
+    const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+    const session      = require('express-session')
+    const MongoStore   = require('connect-mongo')(session);
+    const app = express();
 
-const app = express();
 
 // require database configuration
 require('./configs/db.config');
@@ -29,10 +32,23 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(
+    session({
+      secret: 'my secret',
+      store: new MongoStore({ // store session information in mongo
+        mongooseConnection: mongoose.connection
+      })
+    })
+  )
+
+  
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Basic Authentication';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
+
+const auth = require('./routes/auth');
+app.use('/', auth);
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+
+function requireLogin(req, res, next) {
+    if (req.session.currentUser) {  // we are logged in - req.session.currentUser
+      next(); // this will allow this next route to run
+    } else {
+      res.redirect('/login');
+    }
+}
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/', (req, res, next) => {
+    res.render('index', { user: req.session.currentUser});
+  });
+  
+  router.get('/private', requireLogin, (req, res) => {
+    res.render('private')
+  });
+
+  router.get('/main', requireLogin, (req, res) => {
+    res.render('cutepic')
+  });
 
 module.exports = router;
